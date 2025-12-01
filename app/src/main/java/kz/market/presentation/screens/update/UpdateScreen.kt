536fac1e9658062e4422e162ee2d463df2cbdfb6
dillybert <dillybert@github.com>
2025-12-01 11:22:40 +0500

    fix: Fixing github actions again

--- app/src/main/java/kz/market/presentation/screens/update/UpdateScreen.kt
+++ app/src/main/java/kz/market/presentation/screens/update/UpdateScreen.kt
@@ -55,7 +55,7 @@ fun UpdateScreenContent(
         }
     ) { innerPadding ->
         Text(
-            text = "UpdateScreenContent",
+            text = "UpdateScreenContent preview",
             modifier = Modifier.padding(innerPadding)
         )
     }
